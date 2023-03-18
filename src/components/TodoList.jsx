fec95c89313aea570fda9ddd75a91427d1b891f3
Emir Buckun <emirbuckun@hotmail.com>
2023-03-18 21:51:30 +0300

    Add Todo functionality to TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 
 function TodoList() {
-  const [todos, setTodos] = useState("");
+  const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
@@ -15,10 +16,42 @@ function TodoList() {
     console.log(todo, ...todos);
   };
 
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+
   return (
     <div>
       <h1>What's the plan for today?</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        updateTodo={updateTodo}
+        removeTodo={removeTodo}
+      />
     </div>
   );
 }
