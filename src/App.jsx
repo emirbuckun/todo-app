29af18ba1b66f1b0aff9db41857308243392c9f5
Emir Buckun <emirbuckun@hotmail.com>
2023-03-18 20:55:10 +0300

    Edit TodoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
